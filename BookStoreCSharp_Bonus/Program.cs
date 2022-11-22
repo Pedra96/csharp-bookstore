032caa9a218d7f3d8d59b8e62dce0b2990cf2d83
Pedra96 <darkraidialga@yahoo.it>
2022-11-22 15:48:29 +0100

    inizio test dizionario

--- BookStoreCSharp_Bonus/Program.cs
+++ BookStoreCSharp_Bonus/Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Inserisci titolo libro");
+string TitoloLibro = Console.ReadLine();
+
+Console.WriteLine("Inserisci autore");
+string LibroAutore= Console.ReadLine();
+
+Dictionary<string,string> Libro = new Dictionary<string,string>();
+Libro.Add("titolo", TitoloLibro);
+
+Libro.Add("autore", LibroAutore);
+
+Console.WriteLine(Libro["titolo"]);
+Console.WriteLine(Libro["autore"]);
+
+
+/*
+ * public class Libro
+{
+    string Titolo = "Metro 2035";
+    string Autore = "Dmitry Glukhovsky";
+    string ISBN = "978-5-17-090538-6";
+    float larghezza = 13.8f;
+    float altezza = 20.1f;
+    float profondità = 4.4f;
+    int NumeroPagine = 560;
+    float ValutazioneMedia = 4.5f;
+    int NumeroDiRecensioni = 158;
+    bool kindle = true;
+    bool CopertinaFlessibile = true;
+    float PesoLibro = 0.64f;
+}
+
+  Libro test1 = new Libro();
+ * 
+ * Console.WriteLine($"---------------IL LIBRO DI OGGI: {Titolo} di {Autore} ----------------");
+Console.WriteLine("Informazioni generiche:");
+Console.WriteLine("ISBN: " + ISBN);
+Console.WriteLine("Numero di pagine: " + NumeroPagine);
+Console.WriteLine("Peso del libro: " + PesoLibro + "kg");
+Console.WriteLine(larghezza + "cm x " + altezza + "cm x " + profondità + "cm");
+Console.WriteLine("\nInformazioni Amazon:");
+Console.WriteLine("Numero di recensioni: " + NumeroDiRecensioni + " recensioni");
+Console.WriteLine("Valutazione media: " + ValutazioneMedia + " stellina");
+if (kindle)
+{
+    Console.WriteLine("Kindle disponibile: Si");
+}
+else
+{
+    Console.WriteLine("Kindle disponibile: No");
+}
+if (CopertinaFlessibile)
+{
+    Console.WriteLine("Copertina flessibile disponibile: Si");
+}
+else
+{
+    Console.WriteLine("Copertina flessibile disponibile: No");
+}
+*/
