f40133540ca0705fb363b893c92087bb5cc2ca72
Pedra96 <darkraidialga@yahoo.it>
2022-11-22 14:48:09 +0100

    esercizio finito mi accingo al bonus ciao emanuele :)

--- BookStoreCSharp/Program.cs
+++ BookStoreCSharp/Program.cs
@@ -12,7 +12,39 @@ se la copertina flessibile è disponibile
 */
 
 
-Console.WriteLine("inserire titolo del libro");
-string titolo = Console.ReadLine();
+string Titolo = "Metro 2035";
+string Autore = "Dmitry Glukhovsky";
+string ISBN = "978-5-17-090538-6";
+float larghezza = 13.8f;
+float altezza = 20.1f;
+float profondità = 4.4f;
+float ValutazioneMedia = 4.5f;
+int NumeroDiRecensioni = 158;
+bool kindle = true;
+bool CopertinaFlessibile = true;
+float PesoLibro = 0.64f;
 
-Console.WriteLine("inserire")
+Console.WriteLine($"---------------IL LIBRO DI OGGI: {Titolo} di {Autore} ----------------");
+Console.WriteLine("Informazioni generiche:");
+Console.WriteLine("ISBN: " + ISBN);
+Console.WriteLine("Peso del libro: "+ PesoLibro +"kg");
+Console.WriteLine(larghezza + "cm x " + altezza + "cm x " + profondità + "cm");
+Console.WriteLine("\nInformazioni Amazon:");
+Console.WriteLine("Numero di recensioni: " + NumeroDiRecensioni + " recensioni");
+Console.WriteLine("Valutazione media: " + ValutazioneMedia + " stellina");
+if (kindle)
+{
+    Console.WriteLine("Kindle disponibile: Si");
+}
+else
+{
+    Console.WriteLine("Kindle disponibile: No");
+}
+if (CopertinaFlessibile)
+{
+    Console.WriteLine("Copertina flessibile disponibile: Si");
+}
+else
+{
+    Console.WriteLine("Copertina flessibile disponibile: No");
+}
