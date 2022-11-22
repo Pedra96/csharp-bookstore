d9abd0e6569a0c0fecb23ed12574ed10a19f9d26
Pedra96 <darkraidialga@yahoo.it>
2022-11-22 14:16:54 +0100

    Aggiungere i file di progetto.

---
+++ BookStoreCSharp/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Titolo
+Autore
+ISBN
+Numero pagine
+Peso del libro
+Larghezza, Altezza e Profondità del libro
+Valutazione media
+Numero di recensioni
+se è disponibile in versione Kindle
+se la copertina flessibile è disponibile
+*/
+
+
+Console.WriteLine("inserire titolo del libro");
+string titolo = Console.ReadLine();
+
+Console.WriteLine("inserire")
